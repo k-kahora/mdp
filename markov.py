50da7543b52c82d0bc61d755cb236761782ef86a
Malcolm Kahora <mango22mjk@gmail.com>
2023-05-04 00:15:47 -0400

    Working markovs descsioin process

--- markov.py
+++ markov.py
@@ -7,6 +7,9 @@ def mdp(curentPos, grid_world):
     # print(grid_world)
     living_reward = 0
     goal_reward = 1.00
+
+    print(grid_world)
+
     # Assing states to dict
     for i in range(len(grid_world)):
         for j in range(0, len(grid_world[i])):
@@ -42,9 +45,8 @@ def valid_move(s,a,s_prime):
     # 1 to ignore the floor
     # only go up if state above is a 6
     # Code is reduntdant just trying to get it working
-    down_check = check_bounds(s_prime,  (s_prime[0] - 1, s_prime[1]))
-    right_check = check_bounds(s_prime,  (s_prime[0], s_prime[1] + 1))
-    left_check = check_bounds(s_prime,  (s_prime[0] - 1, s_prime[1] - 1))
+    right_check = check_bounds(s_prime,  (s_prime[0] + 1, s_prime[1]))
+    left_check = check_bounds(s_prime,  (s_prime[0] + 1, s_prime[1]))
     if a == "up":
         if states[s_prime]["tile"] == 6:
             return s_prime
@@ -55,7 +57,7 @@ def valid_move(s,a,s_prime):
         if states[right_check]["tile"] == 4:
             return s_prime
     if a == "down":
-        if states[down_check]["tile"] == 6:
+        if states[s_prime]["tile"] == 6:
             return s_prime
     return s
 
@@ -69,16 +71,18 @@ def check_bounds(s, a):
     # Assume all states prizes are equal
     # Not a typical transition functioin
     # This just takes the given state and action and determinines the next state
+
+    # This a perfect oppuertunity to do a unit test on this funtion
 def T(s, a):
     action = (0,0)
     if a == "up":
-        action = (0,-1)
-    if a == "right":
-        action = (1,0)
-    if a == "left":
         action = (-1,0)
-    if a == "down":
+    if a == "right":
         action = (0,1)
+    if a == "left":
+        action = (0,-1)
+    if a == "down":
+        action = (1,0)
     # s_prime is within the game bounds
     s_prime = (s[0] + action[0], s[1] + action[1])
     # s_prime is no checked within the game logic bounds
@@ -92,26 +96,35 @@ def T(s, a):
 def value_iteration(S, A, gamma=0.9):
     # One to start to see if each iteration is making sense
     # As I gain confidence in the algo I will increase iteratiions 
-    MAX_ITERATIONS = 1 
+    MAX_ITERATIONS = 2
 
     V = {s: 0 for s in S}
 
     count = 0
+    print(T((9,4),"left"))
+    print(states[(11,1)]["tile"])
     while count <= MAX_ITERATIONS:
         # Dynamic programming keep copy of previous calculation
         V_prev = V.copy()
         count += 1
+        count_s = 0
         for s in S:
+            count_s += 1
             Q = {}
             for a in A:
                 s_next = T(s,a)
-                Q[a] = states[s_next]["living_reward"] * gamma * V_prev[s_next]
+                Q[a] = states[s_next]["living_reward"] + gamma * V_prev[s_next]
+
+            # print(s_next)
+            # if count_s == 3:
+            #     return
 
             V[s] = max(Q.values())
+        print(V)
             
     # First iteration will be all zero's
     # Next iteraiton will set rewards based on locatioin of the rewards
-    print(V)
+    # print(V)
 
 def find_goal_and_prizes(grid_world, states):
         
@@ -119,8 +132,9 @@ def find_goal_and_prizes(grid_world, states):
     for i in range(len(grid_world)):
         for j in range(0, len(grid_world[i])):
           # print(grid_world[i][j], end="")
-          if grid_world[i][j] == 8 or  grid_world[i][j] == 9 or grid_world[i][j] == 10:
-            states[(i,j)]["living_reward"] = 1
+          # if grid_world[i][j] == 8 or  grid_world[i][j] == 9 or grid_world[i][j] == 10:
+            if grid_world[i][j] == 10:
+                states[(i,j)]["living_reward"] = 100
         # print("")
           
 
